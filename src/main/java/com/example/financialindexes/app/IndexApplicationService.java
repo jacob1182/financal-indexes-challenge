ec09a8b2211a59ceaeaeeaac6c2c49dbcdbcd5e8
Jose A <jacob1182@gmail.com>
2020-07-06 11:15:45 +0200

    Support statistics snapshot per instruments

--- src/main/java/com/example/financialindexes/app/IndexApplicationService.java
+++ src/main/java/com/example/financialindexes/app/IndexApplicationService.java
@@ -8,14 +8,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Semaphore;
 
 @Service
 @RequiredArgsConstructor
 public class IndexApplicationService {
 
+    public static final String ALL_INSTRUMENTS = "ALL_INSTRUMENTS";
+
     @Getter
-    private StatisticsSnapshot statSnapshot = StatisticsSnapshot.getNewInstance();
+    private final Map<String, StatisticsSnapshot> statSnapshots = new HashMap<>();
 
     private final Semaphore semaphore = new Semaphore(1);
 
@@ -26,7 +30,9 @@ public class IndexApplicationService {
 
         try {
             semaphore.acquire();
-            statSnapshot = statSnapshot.withTick(time, tick);
+            registerTick(tick, time, tick.getInstrument());
+            registerTick(tick, time, ALL_INSTRUMENTS);
+
         } catch (Exception e) {
             throw new IllegalStateException(e.getMessage());
         } finally {
@@ -36,23 +42,36 @@ public class IndexApplicationService {
         return true;
     }
 
+    private void registerTick(Tick tick, long time, String instrument) {
+        var snapshot = statSnapshots.getOrDefault(instrument, StatisticsSnapshot.getNewInstance());
+        snapshot = snapshot.withTick(time, tick);
+        statSnapshots.put(instrument, snapshot);
+    }
+
     @Scheduled(fixedRate = 500)
     public void asyncRecalculateStats() {
         try {
             if (!semaphore.tryAcquire())
                 return;
 
-            statSnapshot = statSnapshot.recalculate(System.currentTimeMillis());
+            statSnapshots.keySet().forEach(instrument ->
+                    statSnapshots.computeIfPresent(instrument,
+                            (__, snapshot) -> snapshot.recalculate(System.currentTimeMillis())));
+
         } finally {
             semaphore.release();
         }
     }
 
     public Statistics getStatistics() {
-        return statSnapshot.getStatistics();
+        return getStatistics(ALL_INSTRUMENTS);
+    }
+
+    public Statistics getStatistics(String instrument) {
+        return statSnapshots.getOrDefault(instrument, StatisticsSnapshot.getNewInstance()).getStatistics();
     }
 
     public void clearTicks() {
-        statSnapshot = StatisticsSnapshot.getNewInstance();
+        statSnapshots.clear();
     }
 }
